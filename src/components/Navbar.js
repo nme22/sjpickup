f064220c3dafa38a0544ef92586fb82d1259c0e1
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2023-07-16 15:05:05 -0400

    adding field

---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+export default function Navbar() {
+   return (
+      <div className="flex flex-row justify-end w-full  p-4 gap-4">
+         <button className="bg-color-violet">Sign In</button>
+         <button className="bg-color-indigo">Register</button>
+      </div>
+   );
+}
